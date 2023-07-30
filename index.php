3c4a2edfdc66bc8535fbe98db1c7ddf7caa1a68d
notmeya <95129068+HaizUber@users.noreply.github.com>
2023-07-30 11:05:41 +0800

    Update index.php
    Added:
    Site animations
    New font 
    
    Fixed:
    Positioning of some elements
    Nav menu
    
    To Fix:
    Burger menu on smaller screens
    damn footer elements dont show up

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>R6 Bud | Home</title>
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> 
+    <title>Val Buddy | Home</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="./style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-1nRniuLCZopli7jVLnJ4zNOw3DYIlXaG71rFhFyHUsCnEJU0EKOM6/48VlDr0I9bwn/klGRhSO65OjfULxAMw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
     <style>
+
+        
         * {
             margin: 0;
             padding: 0;
@@ -19,31 +21,28 @@
             font-size: 62.5%;
         }
 
+        body {
+            background-color: #19192c;
+            font-family: "Poppins", sans-serif;
+            font-size: 1.6rem;
+        }
+
         nav {
             position: fixed;
-            font-family: "Poppins", sans-serif;
             display: flex;
             justify-content: space-around;
             align-items: center;
             min-height: 8rem;
-            background-color: rgba(34, 42, 65, 0.5);
-            background: #333333;  /* fallback for old browsers */
-            background: -webkit-linear-gradient(to right, #dd1818, #333333);  /* Chrome 10-25, Safari 5.1-6 */
-            background: linear-gradient(to right, #dd1818, #333333); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-            text-decoration: none;
             width: 100%;
-            top: 0%;
-            z-index: 1; /* Move the nav menu above the slideshow */
+            background: linear-gradient(to right, #dd1818, #333333);
+            z-index: 1;
             opacity: 0.9;
-            
-            
         }
-
 
         #logo {
             margin-left: 1rem;
             font-size: 3.5rem;
-            color: rgb(231, 231, 231);
+            color: #e7e7e7;
         }
 
         .nav-links {
@@ -51,51 +50,22 @@
             justify-content: space-around;
             font-size: 2.2rem;
             width: 60%;
-            
         }
 
         .nav-links li {
             list-style: none;
-            
-            
         }
 
         .nav-links a {
             text-decoration: none;
             display: inline-block;
-            position: relative;
-            color: rgb(233, 232, 232);
-            
-            
+            color: #e9e8e8;
+            padding: 1rem;
+            border-radius: 4px;
         }
-
-        .nav-links a:before {
-            content: "";
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            width: 100%;
-            height: 2px;
-            background-color: transparent;
-            visibility: hidden;
-            transform: scaleX(0);
-            transition: all 0.3s ease-in-out;
-        }
-
-        .nav-links a:hover:before {
-            visibility: visible;
-            transform: scaleX(1);
-            background-color: rgba(233, 232, 232, 0.7); /* Change link color on hover */
-        }
-
 
         .nav-links a:hover {
-            color: rgba(233, 232, 232, 0.7); /* Change link color on hover */
-            background-color: rgba(0, 0, 0, 0.2); /* Add a semi-transparent background color on hover */
-        }
-
-        body {
-            background-color: rgb(25, 25, 44);
+            background-color: rgba(233, 232, 232, 0.1);
         }
 
         .burger {
@@ -105,28 +75,32 @@
         .burger div {
             width: 25px;
             height: 2px;
-            background-color: rgb(233, 232, 232);
+            background-color: #e9e8e8;
             margin: 5px;
         }
 
-        @media screen and (max-width: 1024px) {
-            .nav-links {
-                width: 60%;
-            }
-        }
-
         @media screen and (max-width: 768px) {
-            body {
-                overflow-x: hidden;
+            .nav-links {
+                display: none;
             }
+
+            .burger {
+                display: block;
+                cursor: pointer;
+            }
+
+            .nav-active {
+                transform: translateX(0%);
+            }
+
             .nav-links {
                 z-index: 2;
                 position: absolute;
-                right: 0px;
+                right: 0;
                 height: 92vh;
                 top: 7vh;
                 display: flex;
-                background-color: rgba(34, 42, 65, 0.9); /* Slightly transparent background color */
+                background-color: rgba(34, 42, 65, 0.9);
                 flex-direction: column;
                 align-items: center;
                 width: 30%;
@@ -134,56 +108,30 @@
                 transition: transform 0.5s ease-in;
             }
 
-            .burger {
-                display: block;
-                cursor: pointer;
+            .burger.active .line1 {
+                transform: rotate(-45deg) translate(-5px, 6px);
+            }
+
+            .burger.active .line2 {
+                opacity: 0;
+            }
+
+            .burger.active .line3 {
+                transform: rotate(45deg) translate(-5px, -6px);
             }
         }
 
-        .nav-active {
-            transform: translateX(0%);
-        }
-
-        #r6-head {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            color: rgb(227, 227, 228);
-            font-family: poppins;
-            font-size: 6rem;
-            transform: translate(-50%, -50%);
-            z-index: 2; /* Move the heading above the slideshow and nav menu */
-        }
-
-        .abt-content {
-            position: absolute;
-            width: 100%;
-            top: 70%;
-            color: rgb(226, 226, 226);
-            font-family: poppins;
-            z-index: 2; /* Move the content above the slideshow and nav menu */
-        }
-
-        #text {
-            position: absolute;
-            width: 70%;
-            top: 50%;
-            left: 50%;
-            font-size: 3rem;
-            transform: translate(-50%, -50%);
-        }
-
-        /* Slideshow styles */
         .slider {
             display: flex;
-            width: 300%; /* Three times the container width to accommodate three slides */
+            width: 300%;
+            transition: transform 1s ease;
         }
 
         .slide {
-            flex: 0 0 100%; /* Divide the container width by the number of slides (3 in this case) */
+            flex: 0 0 100%;
             opacity: 0;
             position: absolute;
-            transition: opacity 1s ease-in-out; /* Add transition for smooth fade-in/fade-out */
+            transition: opacity 1s ease-in-out;
         }
 
         .slide.active {
@@ -196,18 +144,17 @@
             object-fit: cover;
         }
 
-        /* Left and right arrow buttons styles */
         .arrow-btn {
             position: absolute;
             top: 50%;
             transform: translateY(-50%);
             font-size: 24px;
-            color: white;
+            color: #ffffff;
             background-color: rgba(0, 0, 0, 0.5);
             padding: 8px 12px;
             border-radius: 50%;
             cursor: pointer;
-            z-index: 2; /* Move the arrows above the slideshow and nav menu */
+            z-index: 2;
         }
 
         .left-arrow-btn {
@@ -218,55 +165,72 @@
             right: 10px;
         }
 
-        .operators {
-            padding: 6rem 0;
-            text-align: center;
-            color: rgb(226, 226, 226);
-            margin-top: 10vh; /* Adjust margin to move the .operators div below the slideshow */
+        #val-head {
+            position: absolute;
+            top: 45%;
+            left: 50%;
+            color: #e3e3e4;
+            font-size: 4rem;
+            transform: translate(-50%, -50%);
+            z-index: 3;
+            opacity: 0;
+            animation: fadeIn 1.5s ease-in-out 0.5s forwards;
+            scale: 150rem;
         }
 
-        @media screen and (max-width: 768px) {
-            .operators {
-                margin-top: 30vh;
-            }
+        .abt-content {
+            position: absolute;
+            width: 80%;
+            top: 60%;
+            left: 50%;
+            color: #e2e2e2;
+            z-index: 3;
+            opacity: 0;
+            transform: translate(-50%, -50%);
+            animation: fadeIn 1.5s ease-in-out 1s forwards;
         }
 
-
-
-        /* Footer styles */
         footer {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background-color: #333333;
-            height: 100px;
-            position: fixed; /* Change the position to fixed to ensure the footer remains at the bottom of the page */
-            bottom: 0;
-            left: 0; /* Add this line to ensure the footer spans the full width of the viewport */
-            width: 100%;
-            z-index: 3; /* Increase the z-index value to move the footer above other elements */
-            
-        }
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #333333;
+    height: 100px;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    
+}
 
-        .footer-content {
-            display: flex;
-            align-items: center;
-        }
+.footer-content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    
+}
 
-        .footer-content .social-icons {
-            margin-right: 10px;
-        }
+.social-icons {
+    display: flex;
+    align-items: center;
+    z-index: 9999; /* Set a high z-index value to move the footer above other elements */
+}
 
-        .social-icons a {
-            color: rgb(226, 226, 226);
-            font-size: 24px;
-            margin: 0 10px;
-            text-decoration: none;
-            transition: color 0.3s ease;
-        }
+.social-icons a {
+    color: rgb(226, 226, 226);
+    font-size: 24px;
+    margin: 0 10px;
+    text-decoration: none;
+    transition: color 0.3s ease;
+}
+.social-icons a:hover {
+    color: rgba(233, 232, 232, 0.7);
+}
 
-        .social-icons a:hover {
-            color: rgba(233, 232, 232, 0.7);
+        @keyframes fadeIn {
+            0% { opacity: 0; }
+            100% { opacity: 1; }
         }
     </style>
 </head>
@@ -288,7 +252,7 @@
     <div class="slideshow-container">
         <div class="slider">
             <div class="slide active">
-                <img src="images/slide1.jpg" alt="Slide 1">
+                <img src="http://localhost/files/Valbuddy/images/slide1.jpg" alt="Slide 1">
             </div>
             <div class="slide">
                 <img src="images/slide2.jpg" alt="Slide 2">
@@ -302,20 +266,20 @@
         <div class="arrow-btn right-arrow-btn" onclick="nextSlide()">&#10095;</div>
     </div>
 
-    <h2 id="r6-head">Val Buddy</h2>
+    <h2 id="val-head">Val Buddy</h2>
     <div class="abt-content">
         <p id="text">Is an online tactical first-person shooter game developed by Ubisoft. It combines precise shooting mechanics and strategic gameplay to make the perfect tactical shooter.</p>
     </div>
     <footer>
-        <div class="footer-content">
-            <div class="social-icons">
-                <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
-                <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
-                <a href="#" target="_blank"><i class="fa fa-instagram"></i></a>
-            </div>
-            <!--  add more content here if needed -->
+    <div class="footer-content">
+        <div class="social-icons">
+            <a href="#" target="_blank"><i class="fab fa-facebook"></i></a>
+            <a href="#" target="_blank"><i class="fab fa-twitter"></i></a>
+            <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
         </div>
-    </footer>
+        <!--  add more content here if needed -->
+    </div>
+</footer>
     <script>
         let slideIndex = 0;
         const slides = document.getElementsByClassName("slide");
